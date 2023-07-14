df75602b63afe6605483947e3e4bdac1ad53cff9
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-07-14 19:54:40 +0200

    Add dynaming catch all route

---
+++ src/app/[...slug]/page.tsx
@@ -0,0 +1,135 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+import ploneClient from "@plone/client";
+// import { useQuery } from "@tanstack/react-query";
+import { dehydrate, Hydrate } from "@tanstack/react-query";
+import getQueryClient from "../getQueryClient";
+import Data from "../data";
+import axios from "axios";
+import { headers } from "next/headers";
+
+const cli = ploneClient.initialize({
+  apiPath: "http://localhost:8080/Plone",
+});
+
+// export async function getData() {
+//   const res = await fetch("http://localhost:8080/Plone/++api++");
+//   const data = await res.json();
+//   return data;
+// }
+
+export async function getData() {
+  const headersList = headers();
+  const pathname = headersList.get("x-invoke-path") || "";
+  console.log(pathname);
+  const server = "http://localhost:8080/Plone/++api++";
+  const response = await axios.get(`${server}${pathname}`);
+  const data = response.data;
+  return data;
+}
+
+export default async function Home() {
+  const { getContentQuery } = cli;
+  const queryClient = getQueryClient();
+  // await queryClient.prefetchQuery(getContentQuery({ path: "/" }));
+  await queryClient.prefetchQuery(["data"], getData);
+  // console.log(queryClient.queryCache.queries);
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <Hydrate state={dehydratedState}>
+      <main className={styles.main}>
+        <Data />
+
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>src/app/page.tsx</code>
+          </p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{" "}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+
+        <div className={styles.grid}>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Learn <span>-&gt;</span>
+            </h2>
+            <p>
+              Learn about Next.js in an interactive course with&nbsp;quizzes!
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Templates <span>-&gt;</span>
+            </h2>
+            <p>Explore the Next.js 13 playground.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Deploy <span>-&gt;</span>
+            </h2>
+            <p>
+              Instantly deploy your Next.js site to a shareable URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+    </Hydrate>
+  );
+}
