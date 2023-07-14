3c513c0c9b9c6ae868f418156216c410cd655cda
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-07-14 19:53:34 +0200

    Add proxy to backend to avoid CORS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Rewrite to the backend to avoid CORS
+  async rewrites() {
+    return [
+      {
+        source: "/\\+\\+api\\+\\+/:slug*",
+        destination: "http://localhost:8080/Plone/%2B%2Bapi%2B%2B/:slug*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
