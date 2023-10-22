cc12fbc2a77d03bbf898b62f60e0603612e1fc8b
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-10-22 23:35:11 +0200

    Better server settings

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,5 @@
 const settings = {
-  apiPath: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/++api++`
-    : 'http://localhost:3000/++api++',
+  apiPath: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
 };
 
 const config = {
