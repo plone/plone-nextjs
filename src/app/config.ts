59e905d1cbe3d2a84b75a146b40c8189f2d8cb93
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-10-23 09:19:58 +0200

    Try with direct server

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,6 @@
 const settings = {
-  apiPath: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
+  // apiPath: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
+  apiPath: 'https://static.197.123.88.23.clients.your-server.de/api',
 };
 
 const config = {
