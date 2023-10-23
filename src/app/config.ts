c1a0edb9ff3d99b1af33033497031590922fbe55
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-10-23 22:36:10 +0200

    Documentation

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,7 @@
 const settings = {
   apiPath: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? // Vercel does not prepend the schema to the NEXT_PUBLIC_VERCEL_URL automatic env var
+      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : 'http://localhost:3000',
 };
 
